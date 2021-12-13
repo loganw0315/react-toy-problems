4487a671c838694c14337aebecb578425f692b1a
Logan Willis <loganw0315@gmail.com>
2021-12-13 15:20:32 -0600

    Completed FilterStrings component

--- src/components/Topics/FilterString.js
+++ src/components/Topics/FilterString.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 
 class FilterString extends Component {
+    state = {
+        unFilteredArray:[`"Logan was at the park"`,`"Jim was on a lake"`,`"Harry was on a bus"`,`"Blake was on the toilet"`],
+        userInput: "",
+        filteredArray:[]
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            userInput: event.target.value
+        })
+    }
+
+    handleClick = (prop) => {
+        let filteredArray = this.state.unFilteredArray.filter(e=>e.includes(prop))
+        this.setState({
+            filteredArray: filteredArray
+        })
+    }
+
     render() {
         return (
-            <div>
-                <h4></h4>
-                <input></input>
-                <button></button>
-                <span></span>
-                <span></span>
+            <div className="puzzleBox filterStringPB">
+                <h4>Filter String</h4>
+                <span className='puzzleText'>{this.state.unFilteredArray.join(", ")}</span>
+                <input className='inputLine' onChange={this.handleChange}></input>
+                <button className='confirmationButton' onClick={() => this.handleClick(this.state.userInput)}>Submit</button>
+                <span className='resultsBox filterStringRB'>{this.state.filteredArray.join(", ")}</span>
             </div>
             )
     }
